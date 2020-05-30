1f66e7c5ee54e07e9274aed0e9a5e7797f56d4e7
Szymon Borucki <szymon.borucki@gmail.com>
2020-05-30 15:00:36 +0200

    add resolver

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const resolver = require('./resolver');
+const resolver = require('./src/resolver');
 const fs = require('fs');
 const path = require('path');
 const mkdirp = require('mkdirp');
---
+++ src/resolver.js
@@ -0,0 +1,57 @@
+
+function getRelated(file) {
+	if (isSpec(file)) {
+		return specToCode(file);
+	} else {
+		return codeToSpec(file);
+	}
+}
+
+function isSpec(file) {
+	return file.indexOf('_test.rb') > -1;
+}
+
+function codeToSpec(file) {
+	let viewRegex = /erb$|haml$|slim$/
+	let isViewFile = file.match(viewRegex);
+
+	if (isViewFile) {
+		return file
+			.replace('/app/', '/test/')
+			.replace('.haml', '.haml_test.rb')
+			.replace('.erb', '.erb_test.rb')
+			.replace('.slim', '.slim_test.rb');
+	}
+
+	file = file.replace('.rb', '_test.rb');
+
+	let isLibFile = file.indexOf('/lib/') > -1;
+	if (isLibFile) {
+		return file.replace('/lib/', '/test/lib/');
+	}
+
+	return file.replace('/app/', '/test/');
+}
+
+function specToCode(file) {
+
+	let viewRegex = /(.erb|.haml|.slim)_test.rb$/;
+
+	let isViewFile = file.match(viewRegex);
+	if (isViewFile) {
+		return file
+			.replace('_test.rb', '')
+			.replace('/test', '/app');
+	}
+
+	file = file.replace('_test.rb', '.rb');
+
+	let isLibFile = file.indexOf('/test/lib/') > -1;
+	if (isLibFile) {
+		return file.replace('/test/lib/', '/lib/');
+	}
+
+	return file.replace('/test/', '/app/');
+}
+
+module.exports = { getRelated, isSpec, codeToSpec, specToCode }
