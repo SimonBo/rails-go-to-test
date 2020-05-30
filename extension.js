eb41cc9fbdb323b8435be22071fefc7b90545ce2
Szymon Borucki <szymon.borucki@gmail.com>
2020-05-30 16:15:25 +0200

    Use "extension" instead of package name

--- extension.js
+++ extension.js
@@ -39,7 +39,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rails-go-to-test.goToTest', function () {
+	let disposable = vscode.commands.registerCommand('extension.goToTest', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
